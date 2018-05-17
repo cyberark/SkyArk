b7ddf65e0905d272618b865167c63d343dd10888
Asaf Hecht <Hechtov@users.noreply.github.com>
2018-05-17 13:33:20 +0300

    Update AWStrace.ps1

--- AWStrace/AWStrace.ps1
+++ AWStrace/AWStrace.ps1
@@ -43,7 +43,8 @@ $AWStealth = @"
 "@                                   
 
 $Author = @"
-          Author:  Asaf Hecht - @Hechtov
+          Author: Asaf Hecht - @Hechtov
+	          CyberArk Labs
             Future updates via Twitter
 
 ----------------------------------------------------
